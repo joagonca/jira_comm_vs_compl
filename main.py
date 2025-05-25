da6386efa463994ef8aeae408adf4604e88637a4
crazyfacka <gmail@joagonca.com>
2025-05-25 23:22:18 +0100

    Implemented retries and the calculation logic

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ Get completed vs. delivered
 
 import argparse
 from datetime import datetime
+import time
 import requests
-
-import json
 
 PROXIES = {}
 
-JIRA_URL = "https://atc.bmwgroup.net/jira/rest/api/latest"
+JIRA_URL = ""
 MAX_RESULTS = 1000
 
 JIRA_USERNAME = ""
@@ -20,25 +19,39 @@ TEAMS_STRING = ""
 
 SPRINT_CUSTOM_FIELD = "customfield_10000"
 
+DELIVERED_IN_SPRINT = 0
+CARRYOVER_IN_SPRINT = 0
+TOTAL_ISSUES = 0
+
 def jira_request(url, method='GET', data=None):
     """Generic function to call JIRA APIs"""
     headers = {
         'Content-Type': 'application/json'
     }
 
-    response = requests.request(method,
-                                url,
-                                headers=headers,
-                                json=data,
-                                auth=(JIRA_USERNAME, JIRA_PASSWORD),
-                                proxies=PROXIES,
-                                timeout=60)
+    status = 0
+    response = None
 
-    response.raise_for_status()
+    while status != 200:
+        response = requests.request(method,
+                                    url,
+                                    headers=headers,
+                                    json=data,
+                                    auth=(JIRA_USERNAME, JIRA_PASSWORD),
+                                    proxies=PROXIES,
+                                    timeout=60)
+
+        status = response.status_code
+        if status != 200:
+            print(f"\rTimed out, sleeping for 30 seconds...", end="", flush=True)
+            time.sleep(30)
+
     return response.json()
 
 def get_all_issues(project_key):
     """Get all issues for a specific project"""
+    global TOTAL_ISSUES
+
     issues_combo = []
     issues_url = f'{JIRA_URL}/search'
 
@@ -62,6 +75,7 @@ def get_all_issues(project_key):
 
         issues_combo =  issues_combo + response['issues']
 
+    TOTAL_ISSUES = total
     return issues_combo
 
 def parse_sprint_string(sprint_string):
@@ -80,6 +94,9 @@ def parse_sprint_string(sprint_string):
 
 def check_issue_resolution_in_sprint(issue):
     """Validates if issue was solved in the sprint"""
+    global DELIVERED_IN_SPRINT
+    global CARRYOVER_IN_SPRINT
+
     changelog_url = f'{JIRA_URL}/issue/{issue["key"]}?expand=changelog'
     changelog_response = jira_request(changelog_url)
 
@@ -103,8 +120,20 @@ def check_issue_resolution_in_sprint(issue):
         sprint = next((s for s in parsed_sprints if s.get("startDate") and s.get("endDate") and s["startDate"] <= ts <= s["endDate"]), None)
         t["sprint"] = sprint["name"] if sprint else None
 
+    start_sprint = ""
+    end_sprint = ""
     for t in transitions:
-        print(f"{t['timestamp']} | {t['from']} ➜ {t['to']} | Sprint: {t['sprint']}")
+#        print(f"{t['timestamp']} | {t['from']} ➜ {t['to']} | Sprint: {t['sprint']}")
+        if t['to'] == "In Progress":
+            start_sprint = t['sprint']
+        if t['to'] == "Resolved":
+            end_sprint = t['sprint']
+
+    if start_sprint != "":
+        if start_sprint == end_sprint:
+            DELIVERED_IN_SPRINT += 1
+        else:
+            CARRYOVER_IN_SPRINT += 1
 
     return False
 
@@ -117,6 +146,11 @@ parser = argparse.ArgumentParser(
 parser.add_argument('--proxy',
                     dest='proxy',
                     help='If a proxy is to be used to reach out to JIRA')
+
+parser.add_argument('-u', '--url',
+                    dest='url',
+                    required=True,
+                    help='JIRA API URL')
 
 parser.add_argument('-p', '--project',
                     dest='project',
@@ -135,6 +169,8 @@ parser.add_argument('-s', '--secret',
 
 args = parser.parse_args()
 
+JIRA_URL = args.url
+
 if args.proxy is not None:
     PROXIES = {
         'http': f'{args.proxy}',
@@ -150,9 +186,18 @@ with open(args.teams, encoding='utf-8') as f:
 
 try:
     # Fetch all issues from the project
+    print("Fetching issues...")
     issues = get_all_issues(args.project)
+
+    i = 0
     for issue in issues:
+        i += 1
+        print(f"\r{i}/{TOTAL_ISSUES}", end="", flush=True)
         check_issue_resolution_in_sprint(issue)
+    
+    print()
+    ratio = DELIVERED_IN_SPRINT / (DELIVERED_IN_SPRINT + CARRYOVER_IN_SPRINT)
+    print(f"Ratio of CD: {ratio}")
 
 except requests.exceptions.RequestException as e:
     print(f"Error making request to Jira: {e}")
